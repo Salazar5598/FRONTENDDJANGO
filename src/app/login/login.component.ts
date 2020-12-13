c99896939b4664a74d021db83b07b0a86a64b638
daga456 <diegoparedes2@outlook.es>
2020-12-13 15:51:02 -0600

    firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  usuario: string = "";
+  password: string = "";
+  
   constructor() { }
 
   ngOnInit(): void {
